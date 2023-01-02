0cd72771354af934585a174b583e02165b734ce3
celenny <celennycs@gmail.com>
2023-01-02 20:50:34 -0300

    add themes

---
+++ lib/themes/adopt_me_colors.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class AdoptMeColors {
+  static const MaterialColor primaryColor = MaterialColor(
+    0xFFFFFFFF,
+    <int, Color>{
+      50: Color(0xFFFFFFFF),
+      100: Color(0xFFFFFFFF),
+      200: Color(0xFFFFFFFF),
+      300: Color(0xFFFFFFFF),
+      400: Color(0xFFFFFFFF),
+      500: Color(0xFFFFFFFF),
+      600: Color(0xFFFFFFFF),
+      700: Color(0xFFFFFFFF),
+      800: Color(0xFFFFFFFF),
+      900: Color(0xFFFFFFFF),
+    },
+  );
+  static const Color white = Color(0xffffffff);
+  static const Color lightGrey = Color(0xfff2f2f2);
+  static const Color grey = Color(0xffd5d5d5);
+  static const Color darkGrey = Color(0xff6d6d6d);
+  static const Color lightPink = Color(0xffffe6e3);
+  static const Color coralRed = Color(0xffff5f50);
+}
